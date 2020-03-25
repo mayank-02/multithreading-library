9255025c48ab48d9461c1b957b054e5ecfcfd831
mayank_jain <mayank20399@gmail.com>
2020-03-25 22:42:20 +0530

    Added code for thread_join, thread_exit and thread_kill

--- one-one/mthread.c
+++ one-one/mthread.c
@@ -2,7 +2,15 @@
 #include <sys/time.h>
 #include <sys/resource.h>
 #include <sched.h>
+#include <linux/futex.h>
+#include <sys/time.h>
+#include "queue.h"
 #include "mthread.h"
+
+static int initialised = 0;
+static int nproc;
+static rlim_t stack_size;
+static queue * task_q;
 
 static rlim_t get_extant_process_limit(void) {
     struct rlimit limit;
@@ -53,21 +61,65 @@ static void deallocate_stack(void *base, size_t stack_size) {
     munmap(base - page_size, stack_size + page_size);
 }
 
+static inline int futex(int *uaddr, int futex_op, int val) {
+    return syscall(SYS_futex, uaddr, futex_op, val, NULL, NULL, 0);
+}
+
+static int arch_prctl(int code, unsigned long *addr) {
+    return syscall(SYS_arch_prctl, code, addr);
+}
+
+static int tgkill(int tgid, int tid, int sig) {
+    return syscall(SYS_tgkill, tgid, tid, sig);
+}
+
+/**
+ * Obtain information about calling thread. 
+ */
+static mthread *thread_self(void) {
+    uint64_t ptr;
+    
+    int err = arch_pctl(ARCH_GET_FS, &ptr);
+    if(err == -1)
+        return NULL;
+    
+    return (mthread *)ptr;
+}
+
 /**
  * Create a new thread starting at the routine given, which will
  * be passed arg.
  */
 int thread_create(mthread_t *thread, void *(*start_routine)(void *), void *arg) {
+    if(!initialised) {
+        task_q = malloc(sizeof(queue));
+        initialise(task_q);
+        
+        mthread *main = (mthread *) malloc(sizeof(mthread));
+        main->start_routine = main->arg = main->result = NULL;
+        main->tid = gettid();
+        enqueue(main);
+
+        stack_size = get_stack_size();
+        nproc = get_extant_process_limit();
+        initialised = 1;
+    }
+    
+    if(nproc-- == 0) {
+        return EAGAIN;
+    }
     mthread *t = (mthread *) malloc(sizeof(mthread));
     if(t == NULL) {
         printf("Insufficient resources to create another thread.\n");
         return EAGAIN;
     }
-
+    if(thread == NULL || start_routine == NULL || arg == NULL) {
+        return -1;
+    }
     t->start_routine = start_routine;
     t->arg = arg;
 
-    t->size = get_stack_size();
+    t->size = stack_size;
     t->base = allocate_stack(t->size);
     if(t->base == NULL) {
         free(t);
@@ -79,21 +131,27 @@ int thread_create(mthread_t *thread, void *(*start_routine)(void *), void *arg) 
                     CLONE_VM | CLONE_FS | CLONE_FILES | CLONE_SIGHAND |
                     CLONE_THREAD | CLONE_SYSVSEM | CLONE_SETTLS |
                     CLONE_PARENT_SETTID | CLONE_CHILD_CLEARTID,
-                    thread_start,
-                    /* Add more args */
-                    );
+                    t,
+                    &t->condition,
+                    t,
+                    &t->condition);
     if(t->tid == -1) {
-        /* Error checks */
         deallocate_stack(t->base);
         free(t);
         return -1;
     }
 
+    enqueue(task_q, t);
+
     *thread = t->tid;
+
     return 0;
 }
 
-void thread_start(void) {
+void thread_start(void *thread) {
+    mthread *t = (mthread *)thread;
+    t->result = t->start_routine(t->arg);
+    thread_exit(t->result);
 }
 
 /**
@@ -102,6 +160,27 @@ void thread_start(void) {
  * start function.
  */
 int thread_join(mthread_t thread, void **retval) {
+    /* Search in queue for tcb corr to tid */
+    mthread *target = search_by_tid(task_q, thread);
+    if(target == NULL) {
+        return ESRCH;
+    }
+    if(target->joined) {
+        return  EINVAL;
+    }
+
+    target->joined = 1;
+
+    int err = futex(&target->futex_word, FUTEX_WAIT, target->thread_id);
+    if (err == -1) {
+        return err;
+    }
+
+    if(retval) {
+        *retval = target->result;
+    }
+
+    return 0;
 }
 
 /**
@@ -115,10 +194,23 @@ void thread_yield(void) {
  * Exit the calling thread with return value retval.
  */
 void thread_exit(void *retval) {
+    mthread *self = thread_self();
+    thread->result = retval;
+    /* WHAT ELSE TO DO HERE? */
+    return 0;
 }
 
 /**
  * Send signal specified by sig to thread
  */
 int thread_kill(mthread_t thread, int sig) {
+    if(sig == 0)
+        return 0;
+    
+    pid_t tgid = getpid();
+    int err = tgkill(tgid, thread, sig);
+    if(err == -1)
+        return errno;
+
+    return 0;
 }
