8462187bfd3378d7b31e33e89c870666f5d10ff0
mayank_jain <mayank20399@gmail.com>
2020-03-29 21:15:10 +0530

    Implemented mutexes for library

---
+++ one-one/mutex.c
@@ -0,0 +1,63 @@
+#define _GNU_SOURCE
+#include <stdatomic.h>
+#include <assert.h>
+#include <errno.h>
+#include <sys/time.h>
+#include <linux/futex.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+#include "mutex.h"
+
+/** 
+ * atomic_compare_exchange_strong(ptr, oldval, newval)
+ * atomically performs the equivalent of:
+ * if (*ptr == *oldval)
+ *     *ptr = newval;
+ * It returns true if the test yielded true and *ptr was updated.
+ */
+static inline int atomic_cas(int *lock_addr, int expected, int desired) {
+    return atomic_compare_exchange_strong(lock_addr, &expected, desired);
+}
+
+static inline int futex(int *uaddr, int futex_op, int val) {
+    return syscall(SYS_futex, uaddr, futex_op, val, NULL, NULL, 0);
+}
+
+static inline int cmpxchg(int *lock_addr, int expected, int desired) {
+  int *ep = &expected;
+  atomic_compare_exchange_strong(lock_addr, ep, desired);
+  return *ep;
+}
+
+int thread_mutex_init(mthread_mutex_t *mutex) {
+    assert(mutex);
+    *mutex = UNLOCKED;
+    return 0;
+}
+
+int thread_mutex_lock(mthread_mutex_t *mutex) {
+    assert(mutex);
+    int c = cmpxchg(mutex, UNLOCKED, LOCKED);
+    if(c != 0) {
+        do {
+            if(c == 2 || cmpxchg(mutex, LOCKED, LOCKED_WAITING) != 0) {
+                futex(mutex, FUTEX_WAIT, LOCKED_WAITING);
+            }
+        } while((c = cmpxchg(mutex, UNLOCKED, LOCKED_WAITING)) != 0);
+    }
+    return 0;
+}
+
+int thread_mutex_trylock(mthread_mutex_t *mutex) {
+    assert(mutex);
+    return *mutex ? EBUSY : 0;
+}
+
+int thread_mutex_unlock(mthread_mutex_t *mutex) {
+    assert(mutex);
+    if(atomic_fetch_sub(mutex, 1) != 1) {
+        atomic_store(mutex, UNLOCKED);
+        futex(mutex, FUTEX_WAKE, 1);
+    }
+    return 0;
+}
---
+++ one-one/mutex.h
@@ -0,0 +1,19 @@
+#ifndef _SPIN_LOCK_H_
+#define _SPIN_LOCK_H_
+
+#define LOCKED_WAITING   (2u)
+#define LOCKED           (1u)
+#define UNLOCKED         (0u)
+
+/* typedef struct mthread_mutex_t { 
+    int            mx_state;
+    mthread_t      mx_owner;
+    unsigned long  mx_count;
+} mthread_mutex_t; */
+
+typedef int mthread_mutex_t;
+int thread_mutex_init(mthread_mutex_t *mutex);
+int thread_mutex_trylock(mthread_mutex_t *mutex);
+int thread_mutex_lock(mthread_mutex_t *mutex);
+int thread_mutex_unlock(mthread_mutex_t *mutex);
+#endif
