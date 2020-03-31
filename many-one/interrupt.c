3b757687f781c58d1eaa0a8703812849c0299d8e
mayank_jain <mayank20399@gmail.com>
2020-03-31 17:34:51 +0530

    Interrupt disable also blocks external SIGVTALRM; interrupt enable unblocks it;

--- many-one/interrupt.c
+++ many-one/interrupt.c
@@ -1,7 +1,14 @@
 #include "interrupt.h"
+#include <signal.h>
 
 void interrupt_enable(mthread_timer_t *timer) {
-    /* This timer counts down against the user-mode CPU time consumed by the process */
+    /* Unblock SIGVTALRM */
+    sigset_t mask;
+    sigaddset(&mask, SIGVTALRM);
+    sigprocmask(SIG_UNBLOCK, &mask, 0);
+
+    /* Enable timer (counts down against the user-mode CPU time consumed by
+       the process */
     timer->it_value.tv_sec = 0;
     timer->it_value.tv_usec = TIMER;
 	timer->it_interval.tv_sec = 0;
@@ -10,6 +17,12 @@ void interrupt_enable(mthread_timer_t *timer) {
 }
 
 void interrupt_disable(mthread_timer_t *timer) {
+    /* Block SIGVTALRM */
+    sigset_t mask;
+    sigaddset(&mask, SIGVTALRM);
+    sigprocmask(SIG_BLOCK, &mask, 0);
+
+    /* Disable timer */
     timer->it_value.tv_sec = 0;
     timer->it_value.tv_usec = 0;
 	timer->it_interval.tv_sec = 0;
