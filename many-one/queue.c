e9f56e9d0b9e50167ced9cc6ef626cc6b42fafa1
mayank_jain <mayank20399@gmail.com>
2020-04-03 00:25:04 +0530

    Fixed destroy() and used calloc over malloc to suppress valgrind uninitialised errors

--- many-one/queue.c
+++ many-one/queue.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
-#include "queue.h" 
+#include "queue.h"
 
 void initialize(queue *q) {
     q->count = 0;
@@ -18,7 +18,7 @@ int isempty(queue *q) {
 }
 
 void enqueue(queue *q, mthread *thd) {
-    node *tmp = malloc(sizeof(node));
+    node *tmp = calloc(1, sizeof(node));
     tmp->thd = thd;
     tmp->next = NULL;
 
@@ -37,17 +37,17 @@ void enqueue(queue *q, mthread *thd) {
 mthread *dequeue(queue *q) {
     if(isempty(q))
         return NULL;
-    
+
     node *tmp = q->head;
     mthread *n = q->head->thd;
-    
+
     q->head = q->head->next;
     q->count--;
-    
+
     if(q->head == NULL) {
         q->tail = NULL;
     }
-    
+
     free(tmp);
     return(n);
 }
@@ -60,11 +60,11 @@ void display(queue *q) {
     if(isempty(q))
         return;
 
-    node *tmp = q->head;
-    printf("Queue: ");
-    for(int i = 0; i < q->count; i++) {
-        printf("%d  ", tmp->thd->tid);
-        tmp=tmp->next;
+    node *runner = q->head;
+    printf("Queue (%d): ", q->count);
+    while(runner) {
+        printf("%d  ", runner->thd->tid);
+        runner = runner->next;
     }
     printf("\n");
 }
@@ -72,7 +72,7 @@ void display(queue *q) {
 mthread *search_on_tid(queue *q, pid_t tid) {
     if(isempty(q))
         return NULL;
-    
+
     node *runner = q->head;
     while(runner) {
         if (runner->thd->tid == tid) {
@@ -87,8 +87,12 @@ void destroy(queue *q) {
     if(isempty(q))
         return;
 
-    while(q->count--) {
-        dequeue(q);
+    node *curr = q->head, *next;
+    for(int i = 0; i < q->count; i++) {
+        next = curr->next;
+        free(curr->thd);
+        free(curr);
+        curr = next;
     }
     
     return;
