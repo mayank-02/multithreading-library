9870eae4e9daa6e790a4f000e455e2a2b63f027b
mayank_jain <mayank20399@gmail.com>
2020-04-12 16:30:41 +0530

    Real time timer and added doxygen style comments

--- many-one/include/interrupt.h
+++ many-one/include/interrupt.h
@@ -2,6 +2,8 @@
 #define _INTERRUPT_H_
 
 #include <sys/time.h>
+
+/// Timer interval
 #define TIMER   (suseconds_t) 10000
 
 typedef struct itimerval mthread_timer_t;
--- many-one/src/interrupt.c
+++ many-one/src/interrupt.c
@@ -1,22 +1,38 @@
+/** 
+ * @file interrupt.c
+ * @brief To handle timers
+ * @author Mayank Jain
+ * @bug No known bugs
+ */
+
 #include <signal.h>
 #include <stdlib.h>
 #include "interrupt.h"
 
+/**
+ * @brief Enable interrupts
+ * @param[in] timer Pointer to timer
+ * @note The timer is set for TIMER nanoseconds defined in the header 
+ * @note At each expiration of the timer, a SIGALRM is sent
+ */
 void interrupt_enable(mthread_timer_t *timer) {
-    /* Enable timer (counts down against the user-mode CPU time consumed by
-       the process */
+    /* Enable timer (counts down in real time) */
     timer->it_value.tv_sec = 0;
     timer->it_value.tv_usec = TIMER;
 	timer->it_interval.tv_sec = 0;
 	timer->it_interval.tv_usec = TIMER;
-    setitimer(ITIMER_VIRTUAL, timer, 0);
+    setitimer(ITIMER_REAL, timer, 0);
 }
 
+/**
+ * @brief Disable interrupts
+ * @param[in] timer Pointer to timer
+ */
 void interrupt_disable(mthread_timer_t *timer) {
     /* Disable timer */
     timer->it_value.tv_sec = 0;
     timer->it_value.tv_usec = 0;
 	timer->it_interval.tv_sec = 0;
    	timer->it_interval.tv_usec = 0;
-    setitimer(ITIMER_VIRTUAL, timer, 0);
+    setitimer(ITIMER_REAL, timer, 0);
 }
